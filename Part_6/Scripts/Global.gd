da0d5adf93a6774b19777f94e02a4eba41e16bff
christine <christinec.dev@gmail.com>
2023-09-12 10:49:21 +0200

    Add files via upload

---
+++ Part_6/Scripts/Global.gd
@@ -0,0 +1,19 @@
+### Global.gd
+
+extends Node
+
+# Preloaded scenes
+var level_scene = preload("res://Scenes/Level.tscn")
+var player_scene = preload("res://Scenes/Player.tscn")
+var bomb_scene = preload("res://Scenes/Bomb.tscn")
+
+# Game State
+enum GameMode { NORMAL, BATTLE }
+var current_game_mode
+
+# Player variables
+var player
+var color: Array = ["blue", "grey", "orange"]
+
+# Level variables
+var current_level = 1
